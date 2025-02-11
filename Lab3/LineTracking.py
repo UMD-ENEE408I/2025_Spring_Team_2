a722cde0f9bcae8423b4d9f00addd9668abf0e54
Trent Khanjar <tkhanjar@terpmail.umd.edu>
2025-02-11 12:01:22 -0500

    filtered bad lines, probably a lot of huge bugs

--- Lab3/LineTracking.py
+++ Lab3/LineTracking.py
@@ -14,7 +14,7 @@ def detectLine(frame):
     """
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     kernel_size = 5
-    blur_gray = frame # cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+    blur_gray = gray # cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
 
     ret, thresh = cv2.threshold(blur_gray, 200, 255, cv2.THRESH_BINARY)
 
@@ -30,16 +30,31 @@ def detectLine(frame):
     lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
                     min_line_len, max_line_gap)
     center = None
+    str_lines = []
     if lines is not None:   
         for line in lines:
             for x1,y1,x2,y2 in line:
                 if x1 > 480 or x2 > 480 or x1 < 160 or x2 < 160:
                     pass
                 else:
-                    center = (int((x1+x2)/2), int((y2 + y1)/2))
+                    if abs(x1 - x2) < 10:
+                        str_lines.append(line)
+                    # center = (int((x1+x2)/2), int((y2 + y1)/2))
                     # cv2.circle(line_image, center, radius=1, color=(255, 0, 0), thickness=-1)
 
-                    cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+                    # cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+    if len(str_lines) < 2:
+        print("not enough vertical lines")
+    if len(str_lines) >= 2:
+        x11, y11, x21, y21 = str_lines[0][0]
+        x12, y12, x22, y22 = str_lines[1][0]
+        x1 = int((x11 + x12)/2)
+        y1 = int((y11 + y12)/2)
+        x2 = int((x21 + x22)/2)
+        y2 = int((y21 + y22)/2)
+
+
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
     return center, line_image
 
 
