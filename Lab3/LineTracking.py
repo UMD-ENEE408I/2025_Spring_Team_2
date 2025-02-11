ba12b23ca4b90025d816ad180fa25201718c3da9
Trent Khanjar <tkhanjar@terpmail.umd.edu>
2025-02-11 12:40:29 -0500

    'finished' line follower (production standard truthers in shambles rn

--- Lab3/LineTracking.py
+++ Lab3/LineTracking.py
@@ -13,10 +13,13 @@ def detectLine(frame):
         newFrame: Processed frame with the detected line marked using cv2.rectangle() and center marked using cv2.circle().
     """
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    kernel_size = 5
+    kernel = np.ones((5, 5), np.uint8)
     blur_gray = gray # cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
 
     ret, thresh = cv2.threshold(blur_gray, 200, 255, cv2.THRESH_BINARY)
+
+    thresh = cv2.morphologyEx(thresh,cv2.MORPH_OPEN, kernel)
+    thresh = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE, kernel)
 
 
     edges = cv2.Canny(thresh, 150, 50)
@@ -37,29 +40,33 @@ def detectLine(frame):
                 if x1 > 480 or x2 > 480 or x1 < 160 or x2 < 160:
                     pass
                 else:
-                    if abs(x1 - x2) < 10:
-                        str_lines.append(line)
-                    # center = (int((x1+x2)/2), int((y2 + y1)/2))
+                    if abs(x1 - x2) < 30:
+
+                        str_lines.append(line[0])
                     # cv2.circle(line_image, center, radius=1, color=(255, 0, 0), thickness=-1)
 
                     # cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
     if len(str_lines) < 2:
         print("not enough vertical lines")
     if len(str_lines) >= 2:
-        x11, y11, x21, y21 = str_lines[0][0]
-        x12, y12, x22, y22 = str_lines[1][0]
-        x1 = int((x11 + x12)/2)
-        y1 = int((y11 + y12)/2)
-        x2 = int((x21 + x22)/2)
-        y2 = int((y21 + y22)/2)
+        all_x = []
+        all_y = []
+        for (x1, y1, x2, y2) in str_lines:
+            all_x.extend([x1, x2])
+            all_y.extend([y1, y2])
 
+        min_x, max_x = min(all_x), max(all_x)
+        min_y, max_y = min(all_y), max(all_y)
 
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+        center = (int((min_x+max_x)/2), int((min_y + max_y)/2))
+        cv2.circle(line_image, center, radius=1, color=(255, 0, 0), thickness=-1)
+
+        cv2.rectangle(line_image,(min_x,min_y),(max_x,max_y),(255,0,0),5)
     return center, line_image
 
 
 def main():
-    cam = cv2.VideoCapture(0)  # Open webcam
+    cam = cv2.VideoCapture(1)  # Open webcam
 
     while cam.isOpened():
         ret, frame = cam.read()
